4e69f1989015bf26791276754e86675bb7846620
reubenhub2017 <reubenukah2017@gmail.com>
2020-08-08 18:34:56 -0400

    Finish

--- app.py
+++ app.py
@@ -21,25 +21,7 @@ import time
 """Start-up load screen  """
 
 
-def SplashScreen():
-    root = Tk()
-    root.title("Works")
-    image2 = Image.open("load.png")
-    imageFile = ImageTk.PhotoImage(image = image2, size='200x200')
-    progress_bar = ttk.Progressbar(root, orient= 'horizontal', length=300,mode="determinate")
-    progress_bar.config(mode='determinate',maximum=100, value=5)
-    progress_bar.pack(side="bottom")
-    label = Label(master=root,image=imageFile).pack()
-    progress_bar["maximum"] = 100
-    for i in range(101):
-        time.sleep(0.05)
-        progress_bar["value"] = i
-        progress_bar.update()
 
-        progress_bar["value"] =  K0
-    #root.overideredirect(True)
-    progress_bar.start()
-    root.mainloop()
 
 """Starting the Application """
 def start():
